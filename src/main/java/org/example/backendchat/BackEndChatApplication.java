17d49e190f1c96f9128dd17dbecc152a186c0ed9
mingyu <kimmingyu9811@gmail.com>
2024-08-07 13:31:56 +0900

    first commit

---
+++ src/main/java/org/example/backendchat/BackEndChatApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndChatApplication.class, args);
+	}
+
+}
